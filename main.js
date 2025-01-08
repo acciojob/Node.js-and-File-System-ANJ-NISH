793639cea2c9535b262217a887ff6d3a70b248f6
Anurag Joshi <anuragj990@gmail.com>
2025-01-08 15:34:02 +0530

    1st try

--- main.js
+++ main.js
@@ -9,6 +9,44 @@ const rl = readline.createInterface({
 rl.question('Enter the filename: ', (filename) => {
   rl.question('Enter the word to remove: ', (word) => {
     // TODO: Implement this function
+
+    let alldata="";
+    fs.readFile(filename, (err, data)=> {
+      if(err)
+      {
+        console.log(err);
+      }
+      else
+      {
+        
+        alldata+=data.toString();
+
+        let newdata="";
+
+    for(let i=0;i<alldata.length;)
+    {
+      let newword="";
+      while(i<alldata.length && alldata.charAt(i)!==" ")
+      {
+        newword+=alldata.charAt(i);
+        i++;
+      }
+      if(newword!==word)
+      {
+        newdata+=newword;
+      }
+      if(i!==alldata.length)
+      {
+        newdata+=" ";
+      }
+      i++;
+    }
+
+    fs.writeFile(filename,newdata,(err)=> console.log(err));
+        
+      }
+    });
+    
     rl.close();
   });
 });
